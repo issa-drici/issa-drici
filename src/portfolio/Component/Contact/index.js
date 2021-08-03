a6fc0f584092c8a6740df45b24b69f79a0516218
issa-drici <driciissa76@gmail.com>
2021-08-03 17:27:40 +0200

    install dep

--- src/portfolio/Component/Contact/index.js
+++ src/portfolio/Component/Contact/index.js
@@ -67,7 +67,7 @@ const Contact = ({ closeContact }) => {
           </label>
           <input type="submit" value="Envoyer" onClick={() => {
             if (!(toSend.from_email.length < 1 || toSend.from_name.length < 1 || toSend.message.length < 1)) {
-              console.log('d')
+              console.log('dd')
             }
           }}></input>
         </form>
