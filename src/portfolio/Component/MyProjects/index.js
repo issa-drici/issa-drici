31b634667b131e27bf81f8326dda0e2f918977a7
issa-drici <driciissa76@gmail.com>
2021-08-05 12:50:45 +0200

    update

--- src/portfolio/Component/MyProjects/index.js
+++ src/portfolio/Component/MyProjects/index.js
@@ -9,11 +9,6 @@ const MyProjects = () => {
     <div className="projects-section">
       <div className="list-cards">
         <Card image={tipCalculator} name="Tip Calculator" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="React" link="/projects/tip-calculator"/>
-        <Card image={tipCalculator} name="GkCoiff" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="HTML - CSS - PHP - MYSQL" link="/Projects/TipCalculator"/>
-        <Card image={tipCalculator} name="SpeedPizza" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="React" link="/Projects/TipCalculator"/>
-        <Card image={tipCalculator} name="SpeedPizza" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="React" link="/Projects/TipCalculator"/>
-        <Card image={tipCalculator} name="SpeedPizza" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="React" link="/Projects/TipCalculator"/>
-        <Card image={tipCalculator} name="SpeedPizza" textDescription="Ce projet corresponds à un calculateur de pourboire." technos="React" link="/Projects/TipCalculator"/>
       </div>
     </div>
   )
